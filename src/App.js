9534d60cfb6dc34b1510158b2c78ac03f08bf82f
xemnikk <xemnikk@gmail.com>
2020-08-18 15:19:03 -0500

    basic methods done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ProductList from './ProductList'
+import 'bootstrap/dist/css/bootstrap.min.css';
+const url = 'http://localhost:3000/products'
 
-function App() {
+export default class App extends React.Component {
+
+  state = {
+    products: []
+  }
+
+  componentDidMount() {
+    fetch(url)
+    .then(resp => resp.json())
+    .then(json => this.setState({ products: json }))
+  }
+
+  addProduct(event) {
+    fetch(`${url}`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        name: event.target.name.value,
+        description: event.target.description.value,
+        image: event.target.image.value
+      })
+    })
+  }
+
+  deleteProduct = (product) => {
+    fetch(`http://localhost:3000/products/${product.id}`, {
+      method: "DELETE"
+    })
+  }
+
+  editProduct(product, event) {
+    fetch(`http://localhost:3000/products/${product.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        name: event.target.name.value,
+        description: event.target.description.value,
+        image: event.target.image.value
+      })
+    })
+  }
+
+  addLike(product) {
+    fetch(`http://localhost:3000/likes`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        product_id: product.id
+      })
+   })
+  }
+
+  addReview(product, event) {
+    fetch(`http://localhost:3000/reviews`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        text: event.target.text.value,
+        rating: event.target.rating.value,
+        product_id: product.id
+      })
+    })
+  }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProductList products={this.state.products} addProduct={this.addProduct} addLike={this.addLike} addReview={this.addReview} deleteProduct={this.deleteProduct} editProduct={this.editProduct}/>
     </div>
   );
+  }
 }
-
-export default App;
