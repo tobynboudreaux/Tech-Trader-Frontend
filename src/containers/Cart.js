662406b49ac0651187ef8b8121ab6b7edc738e5e
xemnikk <xemnikk@gmail.com>
2020-08-19 17:31:18 -0500

    added total price check to cart

--- src/containers/Cart.js
+++ src/containers/Cart.js
@@ -4,9 +4,22 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button'
 
 export default class Cart extends React.Component {
+
+    totalPrice = (total, num) => {
+        return total + num
+        
+    }
+
     render() {
+        let total
+        const prices = this.props.products.map(prod => {return prod.price})
+        prices.length > 0 ? total = prices.reduce(this.totalPrice) : total = null
+        console.log(total)
         return (
             <div>
+                <br></br><br></br>
+                <h1>Cart Total: {total}</h1>
+                <br></br><br></br>
                 {this.props.products.map((prod) => (
                     <Card style={{ width: '20rem' }}>
                     <Card.Img variant="top" src={prod.image} />
@@ -15,14 +28,16 @@ export default class Cart extends React.Component {
                         <Card.Text>
                         {prod.description}
                         </Card.Text>
-                    
-                        <Card.Link onClick={() => this.props.show(prod)}>Card Info</Card.Link>
-                        <Card.Link href="#">Go To Top</Card.Link>
+            
+                        <Card.Text>
+                            ${prod.price}
+                        </Card.Text>
                     </Card.Body>
                     <Button variant='primary' onClick={() => this.props.removeFromCart(prod)}>Remove From Cart</Button>
                     <br></br><br></br>
-                </Card>                
+                </Card>       
                 ))}
+                
             </div>
         )
     }
